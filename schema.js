b2eb1153750dceabf1f6181847f5492a68ab3d05
osavchenkoNIX <osavchenko@n-ix.com>
2017-08-03 15:24:17 +0300

    json-server and axios added.

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -8,12 +10,12 @@ const {
 } = require('graphql');
 
 // Hardcoded data
-const customers = [
-    { id: '1', name: 'John Doe', email: 'johndoe@gmail.com', age: 35 },
-    { id: '2', name: 'John Bash', email: 'johnbash@gmail.com', age: 12 },
-    { id: '3', name: 'Bob Dylan', email: 'bobdylan@gmail.com', age: 32 },
-    { id: '4', name: 'Jon Snow', email: 'jonsnow@gmail.com', age: 22 }
-];
+// const customers = [
+//     { id: '1', name: 'John Doe', email: 'johndoe@gmail.com', age: 35 },
+//     { id: '2', name: 'John Bash', email: 'johnbash@gmail.com', age: 12 },
+//     { id: '3', name: 'Bob Dylan', email: 'bobdylan@gmail.com', age: 32 },
+//     { id: '4', name: 'Jon Snow', email: 'jonsnow@gmail.com', age: 22 }
+// ];
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -36,17 +38,21 @@ const RootQuery = new GraphQLObjectType({
                 id: { type: GraphQLString }
             },
             resolve(parentValue, args) {
-                for (let i = 0; i < customers.length; i++) {
-                    if (customers[i].id === args.id) {
-                        return customers[i];
-                    }
-                }
+                // for (let i = 0; i < customers.length; i++) {
+                //     if (customers[i].id === args.id) {
+                //         return customers[i];
+                //     }
+                // }
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                    .then(res => res.data);
             }
         },
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args) {
-                return customers;
+                // return customers;
+                return axios.get('http://localhost:3000/customers')
+                    .then(res => res.data);
             }
         }
     }
